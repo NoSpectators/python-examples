b8b650d5bca729865f55d5abf01ecb98b5782f43
John Erickson <john.c.erickson@gmail.com>
2017-02-08 13:03:25 -0500

    added more to it

--- web_scraping.py
+++ web_scraping.py
@@ -1,8 +1,47 @@
-import requests
+#!/usr/bin/env python
+
+"""PYTHON 2 Web Scraping Tutorial
+"""
+
+#!/usr/bin/env python
+
+#this first example uses Python2, urllib, urlopen
+"""
+from urllib import urlopen
 from bs4 import BeautifulSoup
 import re
 
-html  = requests.get("http://menuism.com") 
+html  = urlopen("http://www.menuism.com/restaurant-locations/dominos-pizza-7144") 
+soup = BeautifulSoup(html) #Create instance of beautiful Soup
+print soup.prettify().encode('utf-8')
+"""
+
+##########################################################################################
+#this next example uses the requests module
+"""
+import requests
+from bs4 import BeautifulSoup
+
+html  = requests.get("http://www.menuism.com/restaurant-locations/dominos-pizza-7144") 
 data = html.text   #Store the text from the url into object 'data'
-soup = BeautifulSoup(data, "html.parser") #Create instance of beautiful Soup
-print soup.prettify()
+soup  = BeautifulSoup(data) #Create instance of beautiful Soup
+print soup.prettify().encode('utf-8') #prints all html
+"""
+##########################################################################################
+#this one uses an html parser. 
+"""
+import requests
+from bs4 import BeautifulSoup
+
+html  = requests.get("http://www.menuism.com/restaurant-locations/dominos-pizza-7144") 
+data = html.text   #Store the text from the url into object 'data'
+soup = BeautifulSoup(data, "html.parser")
+print soup.prettify().encode('utf-8') #prints all html
+"""
+##########################################################################################
+"""NOTICE THAT URLOPEN (THE FIRST ONE IS MUCH PRETTIER THAN THE OTHERS. THIS IS BECAUSE
+URLOPEN IS DIFFERENT THAN REQUESTS.
+"""
+
+
+
